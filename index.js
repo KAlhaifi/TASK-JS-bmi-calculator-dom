3c846136d2c19601401e0b34a5c6f6413df36435
khalaf kha <khskh.alh@gmail.com>
2024-10-30 16:58:23 +0300

    Task done

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  let result = weight / Math.pow(height, 2) / 10000;
+  let bmi = result.toFixed(1);
+  let bmiState = "unkown";
+  if (bmi < 18.5) {
+    bmiState = "Underweghit";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    bmiState = "Healthy Weight";
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    bmiState = "Overweight";
+  } else if (bmi > 30) {
+    bmiState = "Above Obesity";
+  }
+  alert(
+    `
+    bmi = ${bmi}
+   bmiState = ${bmiState}`
+  );
 }
